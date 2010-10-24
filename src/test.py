877648c76efd9ef3b90c02ecf169d65c4b7d4624
Sebastian Waisbrot <seppo0010@gmail.com>
2010-10-23 19:06:33 -0700

    Added file stat tests and removed stupid parameter on open

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import stat
+import time
 from git import *
 
 class TestGitFuse(unittest.TestCase):
@@ -12,15 +14,31 @@ class TestGitFuse(unittest.TestCase):
 		self.path = os.getenv('GITPYTHON_TEST_DIR').rstrip('/') + '/';
 		self.repo = Repo(self.repoPath)
 
+        def test_change_file_permissions(self):
+		testtext = 'This is a sample text to write on the file'
+
+                fp = open(self.path + 'permissions', 'w')
+		fp.write(testtext)
+                fp.close()
+		os.chmod(self.path + 'permissions', 0755)
+		statinfo = os.stat(self.path + 'permissions')
+		self.assertTrue(statinfo.st_mode & 0755 == 0755) # There are other flags besides permissions
+		self.assertEqual(statinfo.st_mode & stat.S_IFDIR, 0)
+		self.assertEqual(statinfo.st_mode & stat.S_IFREG, stat.S_IFREG)
+		self.assertEqual(statinfo.st_size, len(testtext))
+		self.assertTrue(time.time() - statinfo.st_atime < 2)
+		self.assertTrue(time.time() - statinfo.st_mtime < 2)
+		self.assertTrue(time.time() - statinfo.st_ctime < 2)
+
 	def test_create_empty_file(self):
-		fp = open(self.path + 'data', 'w', 0755)
+		fp = open(self.path + 'data', 'w')
 		fp.close()
 		repo = Repo(self.repoPath)
 		stats = repo.head.commit.parents[0].stats # HEAD is the file edit - parent is the first commit creating the file
 		self.assertEqual(stats.total['files'], 1)
 
 	def test_edit_file(self):
-		fp = open(self.path + 'data', 'w', 0755)
+		fp = open(self.path + 'data', 'w')
 		fp.write('hello\nworld')
 		fp.close()
 		stats = self.repo.head.commit.stats
